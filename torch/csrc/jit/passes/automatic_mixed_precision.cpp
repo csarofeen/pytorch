e35f9b33a410533d50d3a3af12fd7121acd5f5ff
Lemo <lemo1234@gmail.com>
2021-01-20 11:53:00 -0800

    WIP: autocast pass

--- torch/csrc/jit/passes/automatic_mixed_precision.cpp
+++ torch/csrc/jit/passes/automatic_mixed_precision.cpp
@@ -1,6 +1,7 @@
 
 #include <torch/csrc/jit/passes/automatic_mixed_precision.h>
 
+#include <c10/util/Exception.h>
 #include <torch/csrc/jit/ir/ir.h>
 #include <torch/csrc/jit/jit_log.h>
 
@@ -9,21 +10,40 @@ namespace jit {
 
 namespace {
 
-void handleBlock(Block* block) {
+void handleBlock(Block* block, bool initial_autocast_enabled) {
+  bool autocast_enabled = initial_autocast_enabled;
+
   for (Node* node : block->nodes()) {
-    for (Block* sub_block : node->blocks()) {
-      handleBlock(sub_block);
+    switch (node->kind()) {
+      case prim::CallFunction:
+      case prim::CallMethod:
+        TORCH_INTERNAL_ASSERT(false, "Calls are not supported with AMP & JIT");
+        break;
+      case prim::CreateObject:
+        break;
+      case prim::SetAttr:
+        break;
+      case prim::Enter:
+        break;
+      case prim::Exit:
+        break;
     }
 
-    // TODO
+    // process sub-blocks, if any
+    for (Block* sub_block : node->blocks()) {
+      handleBlock(sub_block, autocast_enabled);
+    }
   }
+
+  // Sanity check: make sure there's no unbalanced transition
+  TORCH_INTERNAL_ASSERT(autocast_enabled == initial_autocast_enabled);
 }
 
 } // namespace
 
 void AutomaticMixedPrecision(const std::shared_ptr<Graph>& graph) {
   GRAPH_DUMP("Before AutomaticMixedPrecision: ", graph);
-  // TODO
+  handleBlock(graph->block(), false);
   GRAPH_DUMP("After AutomaticMixedPrecision: ", graph);
 }
 
